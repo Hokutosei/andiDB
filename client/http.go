0a17642a46960bfe735ce4e0168438f1f74b8fa8
jeanepaul <jeanepaul@gmail.com>
2015-04-28 00:40:42 +0900

    added git ignore, and client http implementation

--- client/http.go
+++ client/http.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 )
 
-func post() {
+// post to server
+func post(cmd string, key string, values []string) {
+	v := url.Values{}
+	v.Set("command", cmd)
+	v.Add("key", key)
 
+	for i, item := range values {
+		hashKey := fmt.Sprintf("%s", string(i))
+		v.Add(hashKey, string(item))
+	}
+
+	fmt.Println(v)
+
+	// resp, err := http.PostForm("http://localhost:8000/post", url.Values{"command": {cmd}, "key": {key}, "values": {values}})
+	resp, err := http.PostForm("http://localhost:8000/post", v)
+	if err != nil {
+		fmt.Println("err post: ", err)
+		return
+	}
+
+	fmt.Println(resp)
 }
 
 func main() {
@@ -15,13 +36,15 @@ func main() {
 	argsWithProg := os.Args
 
 	command := argsWithProg[1]
+	key := argsWithProg[2]
 	fmt.Println(command)
 
 	// values holder
 	values := []string{}
-	for i := 2; i < len(argsWithProg); i++ {
+	for i := 3; i < len(argsWithProg); i++ {
 		values = append(values, argsWithProg[i])
 	}
 
 	fmt.Println(values)
+	post(command, key, values)
 }
