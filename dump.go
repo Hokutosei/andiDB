ce16266dbe3ae37c3a59d874603b5ac6e713572d
jeane paul <jeanepaul@gmail.com>
2015-04-29 01:50:56 +0900

    added file writer to dump.db

--- dump.go
+++ dump.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	_ "io"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"andiDB/storage"
@@ -11,6 +14,8 @@ import (
 
 var (
 	delay = 59
+
+	fileNamePrefix = "dump.db"
 )
 
 // AsyncDumpDb periodically dump db to bson file
@@ -25,14 +30,83 @@ func AsyncDumpDb() {
 func dump() {
 	dbs := storage.AllDb()
 	fmt.Println(dbs)
-	serializeToBson(dbs...)
-}
-
-func serializeToBson(databases ...interface{}) {
-	data, err := bson.Marshal(databases)
+	d, err := serializeToBson(dbs...)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("%q", data)
+	writeToFile(d...)
+	LoadFile()
 }
+
+func serializeToBson(databases ...interface{}) ([]byte, error) {
+	data, err := bson.Marshal(databases)
+	if err != nil {
+		fmt.Println(err)
+		var b []byte
+		return b, err
+	}
+	fmt.Printf("%q", data)
+	return data, nil
+}
+
+func writeToFile(p ...byte) {
+
+	fmt.Println("writing to: ", fileNamePrefix)
+
+	f, err := os.Create(fileNamePrefix)
+	if err != nil {
+
+		// do better error handling here
+		// if cant save file
+		fmt.Println(err)
+		return
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+			panic(err)
+		}
+	}()
+
+	//var i io.Writer
+	n, err := f.Write(p)
+	if err != nil {
+		fmt.Println(n, err)
+	}
+}
+
+func LoadFile() {
+	b, err := ioutil.ReadFile(fileNamePrefix)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(b)
+}
+
+// func LoadFile() {
+// 	fi, err := os.Open(fileNamePrefix)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+//
+// 	defer func() {
+// 		if err := fi.Close(); err != nil {
+// 			fmt.Println(err)
+// 			panic(err)
+// 		}
+// 	}()
+//
+// 	buf := make([]byte, 1024)
+// 	for {
+// 		n, err := fi.Read(buf)
+// 		if err != nil && err != io.EOF {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		fmt.Println(n)
+// 	}
+//
+// }
